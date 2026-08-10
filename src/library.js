400781344eb90ed34b00040264cf464a8f83fb48
tikowikoFamily <mrgamerdu84@gmail.com>
2026-08-10 11:33:36 +0200

    Assembler la bibliothèque

---
+++ src/library.js
@@ -0,0 +1,7 @@
+import { CONTES_1 } from "./data/contes1";
+import { CONTES_2 } from "./data/contes2";
+import { CONTES_3 } from "./data/contes3";
+import { CONTES_4 } from "./data/contes4";
+export { COMPTINES } from "./data/comptines";
+
+export const CONTES = [...CONTES_1, ...CONTES_2, ...CONTES_3, ...CONTES_4];
