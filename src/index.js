b21ce02c9ead1935d94c57e90e58a68a2580af0e
tikowikoFamily <mrgamerdu84@gmail.com>
2026-08-30 00:02:36 +0200

    Charge le style des paroles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './designEnhancements.css';
+import './lyricsStyles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
