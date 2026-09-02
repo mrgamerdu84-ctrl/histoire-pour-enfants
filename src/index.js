35de711ebef07b2bbf32a6e6b186e3425ebfc2de
tikowikoFamily <mrgamerdu84@gmail.com>
2026-09-02 23:32:23 +0200

    Affiche l’écran de démarrage fourni au lancement de l’application

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './designEnhancements.css';
 import './lyricsStyles.css';
+import './launchSplash.css';
+
+function RootApp() {
+  const [showLaunchSplash, setShowLaunchSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = window.setTimeout(() => setShowLaunchSplash(false), 2200);
+    return () => window.clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      {showLaunchSplash && (
+        <div className="launchSplash" aria-label="Écran de démarrage Histoires & Comptines">
+          <img src={`${process.env.PUBLIC_URL}/branding/splash.jpg`} alt="Histoires & Comptines" />
+        </div>
+      )}
+      <App />
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RootApp />
   </React.StrictMode>
 );
