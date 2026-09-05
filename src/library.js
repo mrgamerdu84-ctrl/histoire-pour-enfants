7c5a8c2d6a13f4d183eaae11b744a0133150c7c6
tikowikoFamily <mrgamerdu84@gmail.com>
2026-09-05 22:53:34 +0200

    Ajoute les histoires et berceuses du mode dodo

--- src/library.js
+++ src/library.js
@@ -4,7 +4,14 @@ import { CONTES_3 } from "./data/contes3";
 import { CONTES_4 } from "./data/contes4";
 import { MISSIONS } from "./data/missions";
 import { HISTOIRES_MUSIQUE } from "./data/histoiresMusique";
-export { COMPTINES } from "./data/comptines";
+import { BEDTIME_STORIES } from "./data/bedtimeStories";
+import { COMPTINES as BASE_COMPTINES } from "./data/comptines";
+import { BEDTIME_MUSIC } from "./data/bedtimeMusic";
+
+export const COMPTINES = [
+  ...BASE_COMPTINES,
+  ...BEDTIME_MUSIC,
+];
 
 export const CONTES = [
   ...CONTES_1,
@@ -13,4 +20,5 @@ export const CONTES = [
   ...CONTES_4,
   ...MISSIONS,
   ...HISTOIRES_MUSIQUE,
+  ...BEDTIME_STORIES,
 ];
